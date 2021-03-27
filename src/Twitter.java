1fb2e82889698c595d49921b7f6aa0faea5a0e0f
INDRASHIS PAUL <64627762+IndraP24@users.noreply.github.com>
2021-03-28 00:09:06 +0530

    Added Twitter class

---
+++ src/Twitter.java
@@ -0,0 +1,7 @@
+public class Twitter extends User
+{ //inheritance: inherits the properties and definitions of pet class
+    public Twitter(String name, int age, String gender)
+    {
+        super(name, age, gender);
+    }
+}

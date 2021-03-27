0e34c3817a492b5a92be56c17e2d389143073fd0
INDRASHIS PAUL <64627762+IndraP24@users.noreply.github.com>
2021-03-28 00:08:22 +0530

    Added Instagram class

---
+++ src/Instagram.java
@@ -0,0 +1,15 @@
+public class Instagram extends User
+{ //inheritance: inherits the properties and definitions of pet class
+    public Instagram(String name, int age, String gender)
+    {
+        super(name, age, gender);
+    }
+
+    //polymorphism in java: an object can have many forms
+    //taking selfdescribe() method from pet and creating another form of it!
+    @Override
+    public String selfDescribe() {
+        //here fun cat ready to party is added to the suffix of the ancestor class
+        return super.selfDescribe() + "Super funny and ready to party! "; //super means immediate ancestor
+    }
+}

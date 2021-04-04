7d18f48ecdffaa2755a2612e1c533c028ac19427
Bhushan Mahajan <68232386+bhushanMahajan460@users.noreply.github.com>
2021-04-04 21:11:24 +0530

    Update User.java

--- src/User.java
+++ src/User.java
@@ -14,7 +14,6 @@ public class User
 
     /*
     private int ageMultiplier = 1;
-
     //used inheritance to specialize in pet class for the descendant cat and dog
     public User(String name, int age, String gender, int ageMultiplier)
     {
@@ -32,19 +31,30 @@ public class User
     {
         if (age < 13)
         {
-            return "a kid with the brightest future ahead. ";
+            return "A kid with the brightest future ahead. ";
         }
 
         else if (age >= 13 && age < 20)
         {
-            return "a teenager who loves learning new things. ";
+            return "A teenager who loves learning new things. ";
         }
 
-        else if (age >= 20 && age < 50)
+        else if (age >= 20 && age < 35)
         {
-            return "a working adult of the society. ";
+            return "A responsible youth of the society who wants to do something Big and appreciative !";
+        }
+        
+        else if (age >= 20 && age < 35)
+        {
+        	return "This is your new birth after completing 18 years of your life , do something that resonates .";
+        }
+        
+        
+        else if ( age > 55)
+        {
+        	return "A retired senior citizen of the society always look forward to share my life experience and flaws .  ";
         }
 
-        return "a retired senior citizen of the society. ";
+        return "Females always have to work hard to prove themselves, but they never quit !";
     }
 }

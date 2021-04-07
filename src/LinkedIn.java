6e47976ef31b4f85ec0bb0c956961f3d883b3d5b
Bhushan Mahajan <68232386+bhushanMahajan460@users.noreply.github.com>
2021-04-07 09:17:31 +0530

    Update LinkedIn.java

--- src/LinkedIn.java
+++ src/LinkedIn.java
@@ -1,4 +1,4 @@
-public class LinkedIn extends User
+public class LinkedIn extends User implements genderDescribe
 {
     //inheritance: inherits the properties and definitions of User class
     public LinkedIn(String name, int age, String gender)
@@ -11,5 +11,16 @@ public class LinkedIn extends User
     public String selfDescribe() {
         //immediate ancestor
         return super.selfDescribe() + "Looking forward to connect with everyone!";
+    }
+    
+ public String platformGenDescribe(String gender) {
+    	
+    	if(gender == "male") {
+    		return "Just trying to be better than tomorrow.";
+    	}
+    	
+    	else {
+    		return "I hope the fathers and mothers of girls will look at them and say yes women can .";
+    	}
     }
 }

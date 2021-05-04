af8bdaf435c4d5b3bd4d3e476deb4abb3e5e3803
IndraP24 <indrashis985@gmail.com>
2021-05-04 23:46:00 +0530

    Update Twitter gender based statements

--- src/Twitter.java
+++ src/Twitter.java
@@ -1,26 +1,30 @@
 public class Twitter extends User implements genderDescribe 
 {
-    //inheritance: inherits the properties and definitions of pet class
+    //inheritance: inherits the properties and definitions of User class
     public Twitter(String name, int age, String gender)
     {
         super(name, age, gender);
     }
 
-    //polymorphism
+    //polymorphism: taking selfDescribe() method from User and creating another form of it!
     @Override
     public String selfDescribe() {
-        //immediate ancestor
-        return super.selfDescribe() + "Coffee and Music are my lifeline!";
+        //here a statement is added to the suffix of the ancestor class
+        return super.selfDescribe() + platformGenDescribe (super.getGender ()) + "Coffee and Music are my lifeline!";
     }
-    
+
+    // implementing platformGenDescribe according to gender and platform
     public String platformGenDescribe(String gender) {
     	
-    	if(gender.equals ("male")) {
-    		return "Wants to put more efforts in developement of society";
+    	if(gender.equals ("Male")) {
+    		return "Not your usual guy, love to innovate and inspire. ";
     	}
     	 
-    	else {
-    		return "Want to show Women can do better in the Society .";
+    	else if(gender.equals ("Female")) {
+    		return "Here to inspire that every woman out there can do it if they try. ";
     	}
+
+        else
+            return "";
     }
 }

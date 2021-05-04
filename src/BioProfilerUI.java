0c2ede34c250410dc116d2efb2c7faf613bff959
IndraP24 <indrashis985@gmail.com>
2021-05-04 23:48:25 +0530

    Optimize imports

--- src/BioProfilerUI.java
+++ src/BioProfilerUI.java
@@ -1,7 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class BioProfilerUI extends javax.swing.JFrame {
     private javax.swing.JTextField nameTextField;
